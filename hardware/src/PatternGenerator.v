124ebdfa6880a60773edbd31991b090953769fcc
Kevin_Tyler_McAtee <cs150-ar@c125m-7.EECS.Berkeley.EDU>
2013-10-22 23:13:26 -0700

    Saving now while it works

--- hardware/src/PatternGenerator.v
+++ hardware/src/PatternGenerator.v
@@ -5,43 +5,67 @@ input Reset;
 input VideoReady;
 output reg[23:0] video;
 
-localparam STATE_BLUE = 3'b000,
-		   STATE_GREEN = 3'b001,
+localparam STATE_1 = 3'b000,
+		   STATE_2 = 3'b001,
 		   TURQUOISE = {8'd26, 8'd188, 8'd156},
 		   CARROT = {8'd230, 8'd126, 8'd34},
 		   SUNFLOWER = {8'd241, 8'd196, 8'd15},
 		   POMEGRANATE = {8'd192, 8'd57, 8'd43};
 
 
-reg [6:0] counter;
-reg [2:0] CurrentState;
-reg [2:0] NextState;
+reg [6:0] row_counter;
+reg [2:0] RowState;
+reg [2:0] NextRow;
+
+reg [6:0] column_counter;
+reg [2:0] ColumnState;
+reg [2:0] NextColumn;
 
 always@(posedge Clock) begin 
 	if (Reset) begin
-		CurrentState <= STATE_BLUE;
-		counter <= 7'd0;
+		RowState <= STATE_1;
+		row_counter <= 7'd0;
+		ColumnState <= STATE_1;
+		column_counter <= 7'd0;
 	end
 	else if (VideoReady) begin
-		if (counter == 7'b1001111) begin
-			counter <= 7'd0;
-			CurrentState <= NextState;
+		if (row_counter == 7'b1001111) begin
+			row_counter <= 7'd0;
+			RowState <= NextRow;
 		end
 		else begin 
-			counter <= counter + 7'd1;
+			row_counter <= row_counter + 7'd1;
 		end
 	end
 end
 
 always@(*) begin
-	case (CurrentState)
-		STATE_BLUE: begin
-			video = TURQUOISE;
-			NextState = STATE_GREEN;
+	case (ColumnState)
+		STATE_1: begin
+			case (RowState)
+				STATE_1: begin
+					video = TURQUOISE;
+					NextRow = STATE_2;
+				end
+				STATE_2: begin
+					video = CARROT;
+					NextRow = STATE_1;
+				end
+			endcase
+			NextColumn = STATE_2;
 		end
-		STATE_GREEN: begin
-			video = CARROT;
-			NextState = STATE_BLUE;
+		STATE_2: begin
+			case (RowState)
+				STATE_1: begin
+					video = SUNFLOWER;
+					NextRow = STATE_2;
+				end
+				STATE_2: begin
+					video = POMEGRANATE;
+					NextRow = STATE_1;
+				end
+			endcase
+			NextColumn = STATE_1;
 		end
 	endcase
 end
