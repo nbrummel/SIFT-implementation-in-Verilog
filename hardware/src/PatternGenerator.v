596e5221cb36b7574d7aabd35bf96faf20e26f68
Kevin_Tyler_McAtee <cs150-ar@c125m-7.EECS.Berkeley.EDU>
2013-10-23 00:29:40 -0700

    Started to formulate idea for how to get 4 states

--- hardware/src/PatternGenerator.v
+++ hardware/src/PatternGenerator.v
@@ -7,6 +7,8 @@ output reg[23:0] video;
 
 localparam STATE_1 = 3'b000,
 		   STATE_2 = 3'b001,
+		   STATE_3 = 3'b010,
+		   STATE_4 = 3'b011,
 		   TURQUOISE = {8'd26, 8'd188, 8'd156},
 		   CARROT = {8'd230, 8'd126, 8'd34},
 		   SUNFLOWER = {8'd241, 8'd196, 8'd15},
@@ -16,6 +18,7 @@ localparam STATE_1 = 3'b000,
 reg [6:0] row_counter;
 reg [2:0] RowState;
 reg [2:0] NextRow;
+reg [2:0] NextColumn;
 
 reg [9:0] column_counter;
 
@@ -29,6 +32,7 @@ always@(posedge Clock) begin
 		if (row_counter == 7'b1001111) begin
 			row_counter <= 7'd0;
 			RowState <= NextRow;
+			column_counter <= column_counter + 10'd1;
 		end
 		else begin 
 			row_counter <= row_counter + 7'd1;
@@ -41,10 +45,22 @@ always@(*) begin
 		STATE_1: begin
 			video = TURQUOISE;
 			NextRow = STATE_2;
+			NextColumn = STATE_3;
 		end
 		STATE_2: begin
 			video = CARROT;
 			NextRow = STATE_1;
+			NextColumn = STATE_3;
+		end
+		STATE_3: begin
+			video = SUNFLOWER;
+			NextRow = STATE_4;
+			NextColumn = STATE_1;
+		end
+		STATE_4: begin
+			video = POMEGRANATE;
+			NextRow = STATE_3;
+			NextColumn = STATE_1;
 		end
 	endcase
 end
