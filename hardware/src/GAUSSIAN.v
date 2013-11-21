717ad8e53da28d3621ca2bb489f43c847ab7796c
Nathan Brummel <nbrummel@berkeley.edu>
2013-11-21 06:04:44 -0800

    added new shiftregs and reworked the gaus

--- hardware/src/GAUSSIAN.v
+++ hardware/src/GAUSSIAN.v
@@ -4,29 +4,36 @@ module GAUSSIAN #(
 				K3 = 128,
 				IDLE = 2'b0,
 				SHIFT_STATE = 2'b1)
-		( 	Clk, 
-			din, 
-			Reset, 
-			Clk_en, 
-			dout );
-		
-	input Clk;
-	input [7:0] din;
-	input Reset;
-	input Clk_en;
-	output [7:0] dout;
+		   (input Clk,
+			input [7:0] din,
+			input Reset,
+			input Clk_en,
+			output [7:0] dout);
+	
+	
 
 	wire [15:0] A, B, C, D, E;
 	wire [7:0] horizontal_stage_out;
 	
 	reg [7:0] srA1, srB1;
 	reg [15:0] srC1a, srC1b, srD1a, srD1b;
-	reg Current_State;
+	reg Current_State, Next_State;
 
 	assign A = din * K1;
 	assign B = srA1 * K2;
 	assign C = srB1 * K3;
 	assign E = srD1b;
+
+	initial begin
+		srA1 = 8'h00; 
+		srB1 = 8'h00; 
+		srC1a = 16'h0000;
+		srC1b = 16'h0000;
+		srD1a = 16'h0000; 
+		srD1b = 16'h0000;
+		Current_State = IDLE; 
+		Next_State = IDLE;
+	end
 
 	assign horizontal_stage_out = (A + B + C + D + E) >> 8;
 
@@ -40,26 +47,31 @@ module GAUSSIAN #(
 			srD1b <= 16'd0;
 			if(~Clk_en) 
 				Current_State <= IDLE;
-			else
-				Current_State <= SHIFT_STATE;
 		end
 		else if(~Clk_en) 
 			Current_State <= IDLE; 
-		else
-			Current_State <= SHIFT_STATE;
+		else begin
 			srA1 <= din;
 			srB1 <= srA1;
 			srC1a <= B;
 			srC1b <= srC1a;
 			srD1a <= A;
 			srD1b <= srD1a;
+			Current_State <= Next_State;
+		end
 	end
 
 	always @(*) begin
-		if (Current_State == IDLE) begin
-		end	
-		else begin
-		end
+
+		case (Current_State)
+			IDLE : begin
+				Next_State = IDLE;
+			end
+			SHIFT_STATE : begin
+				Next_State = SHIFT_STATE;
+			end
+		endcase
+			
 	end
 
 
@@ -72,7 +84,7 @@ module GAUSSIAN #(
 	assign dout = (a_2 + b_2 + c_2 + d_2 + e_2) >> 8;
 
 
-	shift_ram_400 srA2 (
+	shiftreg8x400 srA2 (
 		.clk(clock),
 		.ce(Clk_en),
 		.sclr(reset),
@@ -80,7 +92,7 @@ module GAUSSIAN #(
 		.q(shift_out_1_b));
 
 
-	shift_ram_400 srB2 (
+	shiftreg16x400 srB2 (
 		.clk(clock),
 		.ce(Clk_en),
 		.sclr(reset),
@@ -88,7 +100,7 @@ module GAUSSIAN #(
 		.q(shift_out_2_b));
 
 
-	shift_ram_800 srC2 (
+	shiftreg16x800 srC2 (
 		.clk(clock),
 		.ce(Clk_en),
 		.sclr(reset),
@@ -97,7 +109,7 @@ module GAUSSIAN #(
 
 
 
-	shift_ram_800 srD2 (
+	shiftreg16x800 srD2 (
 		.clk(clock),
 		.ce(Clk_en),
 		.sclr(reset),
