1319ba8868f90f54ea5f0de75f6cc2c58a0177e4
Kevin_Tyler_McAtee <cs150-ar@c125m-4.EECS.Berkeley.EDU>
2013-11-24 21:55:53 -0800

    Got it successfully going through two Shift Registers without breaking.

--- hardware/src/GAUSSIAN.v
+++ hardware/src/GAUSSIAN.v
@@ -5,51 +5,20 @@ module GAUSSIAN(
 	input clk_en,
 	output [7:0] dout);
 
-reg[7:0] testReg;
-reg[7:0] testReg2;
-reg[7:0] testReg3;
-reg[7:0] testReg4;
-reg[7:0] testReg5;
-reg[7:0] testReg6;
-reg[7:0] testReg7;
-reg[7:0] testReg8;
-reg[7:0] testReg9;
-reg[7:0] testReg10;
-reg[7:0] testReg11;
-reg[7:0] testReg12;
-reg[7:0] testReg13;
-reg[7:0] testReg14;
-reg[7:0] testReg15;
-reg[7:0] testReg16;
-reg[7:0] testReg17;
+wire [7:0] midStuff;
 
+ShiftReg_8x400 sr1 (
+	.clk(clk),
+	.ce(clk_en),
+	.sclr(rst),
+	.d(din),
+	.q(midStuff));
 
-assign dout = testReg17;
-
-always@(posedge clk) begin
-	if (rst) begin
-		testReg <= 8'd0;
-		testReg2 <= 8'd0;
-	end
-	else begin
-		testReg <= din - 8'b10000000;
-		testReg2 <= testReg;
-		testReg3 <= testReg2;
-		testReg4 <= testReg3;
-		testReg5 <= testReg4;
-		testReg6 <= testReg5;
-		testReg7 <= testReg6;
-		testReg8 <= testReg7;
-		testReg9 <= testReg8;
-		testReg10 <= testReg9;
-		testReg11 <= testReg10;
-		testReg12 <= testReg11;
-		testReg13 <= testReg12;
-		testReg14 <= testReg13;
-		testReg15 <= testReg14;
-		testReg16 <= testReg15;
-		testReg17 <= testReg16;
-	end
-end
+ShiftReg_8x400 sr2 (
+	.clk(clk),
+	.ce(clk_en),
+	.sclr(rst),
+	.d(midStuff),
+	.q(dout));
 
 endmodule
--- hardware/src/GaussianWrapper.v
+++ hardware/src/GaussianWrapper.v
@@ -15,22 +15,28 @@ module GaussianWrapper(
 localparam 	STATE_IDLE = 3'd1,
 			STATE_WRITE = 3'd2,
 			STATE_BUFFER = 3'd3;
+
+localparam shift_value = 10'd799;
+
 reg [2:0] CurrentState;
 reg [2:0] NextState;
 
 wire [7:0] gauss_in;
 wire [7:0] gauss_out;
 reg [8:0] counter;
+reg [9:0] shiftCounter;
 wire write_gauss;
+wire write_FIFO;
 assign rd_en_down = (CurrentState == STATE_WRITE) & valid;
-assign gauss_in = (CurrentState == STATE_BUFFER) ? 8'b11111111 : din;
-assign write_gauss = (CurrentState != STATE_IDLE) & valid;
+assign gauss_in = (CurrentState == STATE_BUFFER) ? 8'd0 : din;
+assign write_gauss = (CurrentState != STATE_IDLE) & valid;// & (shiftCounter == shift_value);
+assign write_FIFO = write_gauss & (shiftCounter == shift_value);
 
 GAUSSIAN g(
 	.clk(clk),
 	.din(gauss_in),
 	.rst(rst),
-	.clk_en(1'b1),
+	.clk_en(write_gauss), //only shifts through if valid
 	.dout(gauss_out));
 
 DOWN_SAMPLE_FIFO dsf(
@@ -38,7 +44,7 @@ DOWN_SAMPLE_FIFO dsf(
 	.rst(rst),
 	.wr_clk(clk),
 	.din(gauss_out),
-	.wr_en(write_gauss), //my logic
+	.wr_en(write_FIFO), //my logic
 	.full(), //need to take care of this
 	//To Gaussian Module
 	.empty(empty),
@@ -51,11 +57,14 @@ always@(posedge clk) begin
 	if (rst) begin
 		CurrentState <= STATE_IDLE;
 		counter <= 9'd0;
+		shiftCounter <= 10'd0;
 	end
 	else begin
 		CurrentState <= NextState;
 		if (counter == 9'd400)
 			counter <= 9'd0;
+		if ((shiftCounter < shift_value) & write_gauss)
+			shiftCounter <= shiftCounter + 10'd1;
 		//else if (NextState != STATE_IDLE)
 			//counter <= counter + 9'd1;
 	end
