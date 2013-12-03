6cafdebc8ab007eb6ff4cc741b902aaec5742c07
Kevin_Tyler_McAtee <cs150-ar@c125m-4.EECS.Berkeley.EDU>
2013-12-02 21:26:50 -0800

    Slow stripping away the gausswrapper

--- hardware/src/GaussianWrapper.v
+++ hardware/src/GaussianWrapper.v
@@ -12,14 +12,7 @@ module GaussianWrapper(
 	output empty,
 	input rd_en_up);
 
-localparam 	STATE_IDLE = 3'd1,
-			STATE_WRITE = 3'd2,
-			STATE_BUFFER = 3'd3;
-
 localparam shift_value = 10'd806;
-
-reg [2:0] CurrentState;
-reg [2:0] NextState;
 
 wire [7:0] gauss_in;
 wire [7:0] gauss_out;
@@ -27,11 +20,10 @@ reg [8:0] counter;
 reg [9:0] shiftCounter;
 wire write_gauss;
 wire write_FIFO;
-assign rd_en_down = (CurrentState == STATE_WRITE) & valid;
-assign gauss_in = (CurrentState == STATE_BUFFER) ? 8'd0 : din;
-assign write_gauss = (CurrentState != STATE_IDLE) & valid;// & (shiftCounter == shift_value);
-assign write_FIFO = write_gauss & (shiftCounter == shift_value);// & (counter > 8'd2);
-wire [7:0] mid_gauss;
+assign rd_en_down = valid;
+assign gauss_in = din;
+assign write_gauss = valid;
+assign write_FIFO = write_gauss & (shiftCounter == shift_value);
 
 GAUSSIAN g1(
 	.clk(clk),
@@ -56,12 +48,10 @@ DOWN_SAMPLE_FIFO dsf(
 
 always@(posedge clk) begin
 	if (rst) begin
-		CurrentState <= STATE_IDLE;
 		counter <= 9'd0;
 		shiftCounter <= 10'd0;
 	end
 	else begin
-		CurrentState <= NextState;
 		if (counter == 9'd400)
 			counter <= 9'd0;
 		if ((shiftCounter < shift_value) & write_gauss)
@@ -69,35 +59,6 @@ always@(posedge clk) begin
 		//else if (NextState != STATE_IDLE)
 			//counter <= counter + 9'd1;
 	end
-end
-
-always @(*) begin
-	case (CurrentState)
-		STATE_IDLE: begin
-			if (counter == 9'd399)
-				NextState = STATE_BUFFER;
-			else if (valid)
-				NextState = STATE_WRITE;
-			else
-				NextState = STATE_IDLE;
-		end
-		STATE_WRITE: begin
-			if (counter == 9'd399)
-				NextState = STATE_BUFFER;
-			else if (valid)
-				NextState = STATE_WRITE;
-			else
-				NextState = STATE_IDLE;
-		end
-		STATE_BUFFER: begin
-			if (counter == 9'd400)
-				NextState = STATE_BUFFER;
-			else if (valid)
-				NextState = STATE_WRITE;
-			else
-				NextState = STATE_IDLE;
-		end
-	endcase
 end
 
 endmodule
